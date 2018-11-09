e0888175568223ea03f93f440eebc0999fb637d1
MaxFedotov <m.a.fedotov@gmail.com>
2018-11-09 14:07:32 +0300

    fix bug in types that int[0,1] were boolean

--- types/user.pp
+++ types/user.pp
@@ -1,13 +1,13 @@
 # lint:ignore:2sp_soft_tabs
 type Proxysql::User = Array[Hash[String, Struct[{ password                         => String[1],
                                                   default_hostgroup                => Integer,
-                                                  Optional[active]                 => Boolean,
-                                                  Optional[use_ssl]                => Boolean,
+                                                  Optional[active]                 => Integer[0,1],
+                                                  Optional[use_ssl]                => Integer[0,1],
                                                   Optional[default_schema]         => String[1],
-                                                  Optional[schema_locked]          => Boolean,
-                                                  Optional[transaction_persistent] => Boolean,
-                                                  Optional[fast_forward]           => Boolean,
-                                                  Optional[backend]                => Boolean,
-                                                  Optional[frontend]               => Boolean,
+                                                  Optional[schema_locked]          => Integer[0,1],
+                                                  Optional[transaction_persistent] => Integer[0,1],
+                                                  Optional[fast_forward]           => Integer[0,1],
+                                                  Optional[backend]                => Integer[0,1],
+                                                  Optional[frontend]               => Integer[0,1],
                                                   Optional[max_connections]        => Integer, }],1,1]]
 # lint:endignore                                                   
